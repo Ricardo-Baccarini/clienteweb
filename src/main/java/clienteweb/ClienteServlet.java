615bf329d5191de20716133258161f573af0dfa1
Ricardo-Baccarini <ricardo.baccarini@gmail.com>
2019-04-16 09:47:40 -0300

    Aula 06

--- src/main/java/clienteweb/ClienteServlet.java
+++ src/main/java/clienteweb/ClienteServlet.java
@@ -11,11 +11,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns={"/cliente", "/clienteServlet", "/clienteController"}) // Mapeado 3 formas de URL	
 public class ClienteServlet extends HttpServlet {
 	
+	   public ClienteServlet() {
+	    	System.out.println("Construindo o Servlet");
+	    }
+		
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+    @Override
+    public void init() throws ServletException {
+    	System.out.println("Iniciando Servlet....");
+    	super.init();
+    }
+    
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	System.out.println("chamando o service....");
+    	super.service(req, resp);
+    }
+    
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("Chamou pelo método GET");
@@ -40,5 +56,11 @@ public class ClienteServlet extends HttpServlet {
 		//System.out.println("Chamou pelo método POST");
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().print("Chamou pelo método PUT");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("Passando pelo destroy....");
+		super.destroy();
 	}
 }
