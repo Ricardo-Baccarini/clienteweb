39324c924d64c17f15ba4255752de3a6a601fd0c
Ricardo-Baccarini <ricardo.baccarini@gmail.com>
2019-04-16 14:50:30 -0300

    Aula 09

--- src/main/java/clienteweb/ClienteServlet.java
+++ src/main/java/clienteweb/ClienteServlet.java
@@ -1,66 +1,96 @@
 package clienteweb;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.activation.MailcapCommandMap;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Dispatch;
 
-@WebServlet(urlPatterns={"/cliente", "/clienteServlet", "/clienteController"}) // Mapeado 3 formas de URL	
+import br.com.fabricadeprogramador.model.Cliente;
+
+@WebServlet(urlPatterns = { "/cliente", "/clienteServlet", "/clienteController" }) // Mapeado 3 formas de URL
 public class ClienteServlet extends HttpServlet {
-	
-	   public ClienteServlet() {
-	    	System.out.println("Construindo o Servlet");
-	    }
-		
+
+	List<Cliente> lista = new ArrayList();
+
+	public ClienteServlet() {
+		System.out.println("Construindo o Servlet");
+	}
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-    @Override
-    public void init() throws ServletException {
-    	System.out.println("Iniciando Servlet....");
-    	super.init();
-    }
-    
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	System.out.println("chamando o service....");
-    	super.service(req, resp);
-    }
-    
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Iniciando Servlet....");
+		super.init();
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("chamando o service....");
+		super.service(req, resp);
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("Chamou pelo método GET");
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("cliente.jsp");
+
+		req.setAttribute("lista", lista); // Atribui ao requeste o objeto lista setando como nome "lista"
+		dispatcher.forward(req, resp); // Encaminha para tela do do cliente
+
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//System.out.println("Chamou pelo método POST");
-		
+		// System.out.println("Chamou pelo método POST");
+
+		// Recebe o email
 		resp.setCharacterEncoding("UTF-8");
 		String email = req.getParameter("e-mail");
-		resp.getWriter().print("Chamou pelo método POST - E-mail " + email + "!");
+
+		// Instancia o cliente
+		Cliente cli = new Cliente();
+		cli.setEmail(email);
+
+		// Adiciona o cliente em uma lista
+		lista.add(cli);
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("cliente.jsp");
+		req.setAttribute("msg", "Cadastrado com sucesso");
+		req.setAttribute("lista", lista); // Atribui ao requeste o objeto lista setando como nome "lista"
+		dispatcher.forward(req, resp); // Encaminha para tela do do cliente
+		
+		//resp.sendRedirect("cliente"); // Refaz requisição para o Servidor
+		
+		// resp.setCharacterEncoding("UTF-8");
+		// resp.getWriter().print("Chamou pelo método POST - E-mail " + email + "!");
 	}
-	
+
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//System.out.println("Chamou pelo método POST");
+		// System.out.println("Chamou pelo método POST");
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().print("Chamou pelo método DELETE");
 	}
-	
+
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//System.out.println("Chamou pelo método POST");
+		// System.out.println("Chamou pelo método POST");
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().print("Chamou pelo método PUT");
 	}
-	
+
 	@Override
 	public void destroy() {
 		System.out.println("Passando pelo destroy....");
---
+++ src/main/webapp/cliente.jsp
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@page import="br.com.fabricadeprogramador.model.Cliente"%>
+<%@page import="java.util.List"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<div>
+		<%
+		Object msg = request.getAttribute("msg");
+		if (msg!=null){
+			String msgStr = (String)msg;
+			out.print(msgStr);
+		}
+		%>
+	</div>
+	<form method="post" action="cliente">
+		E-mail: <input type="text" value="" name="e-mail" /> <input
+			type="submit" value="Save">
+	</form>
+
+	<%
+		List<Cliente> lista = (List<Cliente>) request.getAttribute("lista");
+		for (Cliente c : lista) {
+			out.print(c.getEmail() + "<br/>");
+		}
+	%>
+
+</body>
+</html>
